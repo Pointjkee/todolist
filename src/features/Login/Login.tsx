99fa148383f486ea5a42125e65a1db4b0c92cb19
Denis <razoranet@gmail.com>
2021-12-16 09:58:15 +0300

    update todolist. Final

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useFormik} from 'formik';
+import {useDispatch, useSelector} from "react-redux";
+import {loginTC} from "./authReducer";
+import {AppRootStateType} from "../../app/store";
+import {Navigate} from "react-router-dom";
+
+export const Login = () => {
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            dispatch(loginTC(values))
+            formik.resetForm()
+        },
+    })
+    if (isLoggedIn) {
+        return <Navigate to='/'/>
+    }
+
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+            <FormControl>
+                <FormLabel>
+                    <p>To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'}
+                           target={'_blank'}> here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                </FormLabel>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                        <TextField label="Email"
+                                   margin="normal"
+                                   onChange={formik.handleChange}
+                                   value={formik.values.email}
+                                   name='email'
+                        />
+                        <TextField type="password"
+                                   label="Password"
+                                   margin="normal"
+                                   onChange={formik.handleChange}
+                                   value={formik.values.password}
+                                   name='password'
+                        />
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox/>}
+                                          onChange={formik.handleChange}
+                                          checked={formik.values.rememberMe}
+                                          name='rememberMe'
+                        />
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </form>
+
+            </FormControl>
+        </Grid>
+    </Grid>
+}
+
