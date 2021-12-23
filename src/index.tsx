01483ae77b188a7a096df825e5f7338f639cf29b
Denis <razoranet@gmail.com>
2021-12-23 19:21:37 +0300

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-         <BrowserRouter>
-             <App/>
-         </BrowserRouter>
-        </Provider>
+        <HashRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
